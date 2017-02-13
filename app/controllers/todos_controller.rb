946c8270cb19ddeda099ad5159aa6e863057d283
ShawnGChristy <shawn.g.christy@gmail.com>
2017-02-13 13:36:02 -0600

    add basic list functions

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,30 @@
+class TodosController < ApplicationController
+  before_action :set_todo, only: [:destroy]
+
+  def index
+  @todos = Todo.all
+  @todo = Todo.new
+end
+
+  def create
+    @todo = Todo.new(todo_params)
+    @todo.save
+    redirect_to todos_url
+  end
+
+  def destroy
+    @todo.destroy
+    redirect_to todos_url
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def todo_params
+      params.require(:todo).permit(:name)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'todos#index'
+  get 'todos', to: 'todos#index'
+  post 'todos', to: 'todos#create'
+  delete 'todos/:id', to: 'todos#destroy', as: :todo #as: :todo lets us use todo_path variable
 end
