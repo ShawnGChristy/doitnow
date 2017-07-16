99f5ad6c91651b50a9111e0f4cac7cf9240da619
ShawnGChristy <shawn.g.christy@gmail.com>
2017-07-16 18:53:20 -0500

    revert another error

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,10 @@
 class TodosController < ApplicationController
-
-  before_action :all_todos, only: [:index, :create, :update]
-  before_action :set_todo, only: [:edit, :update, :destroy]
+  
+  before_action :all_tasks, only: [:index, :create, :update]
+  before_action :set_task, only: [:edit, :update, :destroy]
 
   def update
-     @todo.update(todo_params) 
+     @task.update(task_params) 
   end
 
   def index
